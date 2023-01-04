9261f9b2e29f6af04da715289b7a57157f496deb
Daniel Aguiar <dc03934@gmail.com>
2023-01-04 15:24:30 -0300

    Edited

--- src/components/screens/games/DancaCadeira/index.tsx
+++ src/components/screens/games/DancaCadeira/index.tsx
@@ -10,96 +10,199 @@ export default function DancaCadeira ({ route, navigation }) {
   
   const { game } = route.params;
 
-  const [ gameData, setGameData ] = React.useState(game);
-  const [ sound, setSound ]: any = React.useState();
-  const [ gaming, setGaming ]: any = React.useState(false);
+  const [ numChairs, setNumChairs ]: any = React.useState(10);
+  const [ minPauseTime, setMinPauseTime ]: any = React.useState(15);
+  const [ maxPauseTime, setMaxPauseTime ]: any = React.useState(20);
+  const [ waitingTimed, setWaitingTimed ]: any = React.useState(7);
+  const [ gaming, setGaming ] = React.useState(false);
+  const [ paused, setPaused ] = React.useState(true);
+  const [ timePaused, setTimePaused ]: any = React.useState();
+  const [ pausingIn, setPausingIn ]: any = React.useState();
+  const [ loaded, setLoaded ] = React.useState(false);
+  const [ loading, setLoading ] = React.useState(false);
+  const music = React.useRef(new Audio.Sound());
+
+  const SampleTrack = require('../../../../assets/audios/songs/song-1.mp3');
+  
+  var songCurrentTime = 0;
+  var chairs = numChairs;
   
   function getRandomInt(min: number, max: number) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
   }
+
+  React.useEffect(() => {
+    LoadAudio();
+    setGaming(false);
+    setPaused(true);
+  }, []);
+
+  const PlayAudio = async () => {
+    try {
+      const result = await music.current.getStatusAsync();
+      if (result.isLoaded) {
+        if (result.isPlaying === false) {
+          music.current.playAsync();
+        }
+      }
+    } catch (error) {}
+  };
+
+  const PauseAudio = async () => {
+    try {
+      const result = await music.current.getStatusAsync();
+      if (result.isLoaded) {
+        if (result.isPlaying === true) {
+          music.current.pauseAsync();
+        }
+      }
+    } catch (error) {}
+  };
+
+  const StopAudio = async () => {
+    try {
+      const result = await music.current.getStatusAsync();
+      if (result.isLoaded) {
+        if (result.isPlaying === true) {
+          music.current.stopAsync();
+        }
+      }
+    } catch (error) {}
+  };
+
+  const LoadAudio = async () => {
+    setLoading(true);
+    const checkLoading = await music.current.getStatusAsync();
+    if (checkLoading.isLoaded === false) {
+      try {
+        const result = await music.current.loadAsync(SampleTrack, {}, true);
+        if (result.isLoaded === false) {
+          setLoading(false);
+          console.log('Error in Loading Audio');
+        } else {
+          setLoading(false);
+          setLoaded(true);
+        }
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    } else {
+      setLoading(false);
+    }
+  };
   
-  var songCurrentTime = 0;
+    
+  var times = undefined;
+  
+  const timeForPause = () => {
+    setTimeout(() => {
+      setPausingIn(times - 1);
+      times = times - 1;
+      if (paused && times > 0) timeForPause();
+    }, 1000);
+  };
 
-  async function playing(numCadeiras: number, minTime: number, maxTime: number) {
-    var time: any = getRandomInt(minTime, maxTime);
-    const { sound } = await Audio.Sound.createAsync( require('../../../../assets/audios/songs/song-1.mp3') );
+  var timePause = undefined;
 
+  const timeInPaused = () => {
+    setTimeout(() => {
+      setTimePaused(timePause - 1);
+      timePause = timePause - 1;
+      if (paused && timePause > 0) timeInPaused();
+    }, 1000);
+  };
 
-    if (gaming) {
+  React.useEffect(() => {
+    console.log('\n');
+    console.log('Pausado? ' + paused);
+    console.log('Jogando? ' + gaming);
+    console.log('\n');
+  }, [gaming]);
+
+  const PlayGame = () => {
+    setGaming(true);
+    setPaused(false);
+
+    var time: any = getRandomInt(minPauseTime, maxPauseTime);
+    
+    times = time;
+
+    setPausingIn(time);
+    timeForPause();
+
+    if (!gaming) {
       console.log('\n');
       console.log('Tempo: ' + time * 1000);
-      console.log('Cadeiras: ' + numCadeiras);
+      console.log('Cadeiras: ' + numChairs);
       console.log('Current time: ' + songCurrentTime);
       
-      if (songCurrentTime !== 0) {
-        await sound.playFromPositionAsync(songCurrentTime);
-      } else {
-        await sound.playAsync();
-      }
+      PlayAudio();
   
       console.log('Tocando música');
       
-      setTimeout(() => {
-        sound.pauseAsync();
-        console.log('Música pausada');
-  
-        if (songCurrentTime !== 0) {
-          songCurrentTime = songCurrentTime + time * 1000;
-        } else {
-          songCurrentTime = time * 1000;
-        }
-  
-        numCadeiras = numCadeiras - 1;
-        
-        if (numCadeiras >= 1) {
-          console.log('Retirando cadeiras');
-          if (gaming) setTimeout(() =>{
-            playing(numCadeiras, minTime, maxTime);
-          }, 7000);
-        } else {
-          console.log('\n');
-          setGaming(false);
-          console.log('Fim do Jogo!');
-        }
-      }, time * 1000);
+      if (!gaming) {
+        setTimeout(() => {
+          PauseAudio();
+          setPaused(true);
+          timePause = waitingTimed;
+          timeInPaused();
+          console.log('Música pausada');
+    
+          if (songCurrentTime !== 0) {
+            songCurrentTime = songCurrentTime + time * 1000;
+          } else {
+            songCurrentTime = time * 1000;
+          }
+          
+          if (!gaming) {
+            if (numChairs >= 1) {
+              chairs = chairs - 1;
+              setNumChairs(chairs);
+              console.log('Retirando cadeiras');
+              console.log('Tempo de espera: ' + waitingTimed + 's');
+              if (!gaming) {
+                setTimeout(() => {
+                  PlayGame();
+                  setGaming(true);
+                }, waitingTimed * 1000);
+              }
+            } else {
+              console.log('\n');
+              StopAudio();
+              setGaming(false);
+              console.log('Fim do Jogo!');
+            }
+          }
+        }, time * 1000);
+      }
     } else {
-      await sound.pauseAsync();
+      StopAudio();
+      setGaming(false);
+      setPaused(true);
       console.log('Fim do Jogo!');
       songCurrentTime = 0;
       console.log('Current time: ' + songCurrentTime);
     }
   }
 
-  const play = () => {
-    // var numCadeiras: number = 10;
-    setGaming(true);
-    console.log('Jogando: ' + gaming);
-    // playing(numCadeiras, 10, 15);
-  }
+  // React.useEffect(() => {
 
-  const stop = () => {
-    setGaming(false);
-    console.log('Jogando: ' + gaming);
-  }
+  //   console.log('\n');
+  //   console.log('Jogando: ' + gaming);
+  //   console.log('Cadeiras: ' + numChairs);
+  //   console.log('\n');
 
-  React.useEffect(() => {
-    console.log('Jogando: ' + gaming);
-    return sound
-      ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync();
-        }
-      : undefined;
-  }, [sound]);
+  // }, [gaming, numChairs]);
 
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       {/* Header */}
       <View style={[styles.header, {
-        backgroundColor: gameData.theme
+        backgroundColor: game.theme
       }]}>
         <View style={styles.headerRow}>
           <TouchableOpacity 
@@ -109,7 +212,7 @@ export default function DancaCadeira ({ route, navigation }) {
           >
             <Ionicons style={styles.headerButtonIcon} name="ios-chevron-back" />
           </TouchableOpacity>
-          <Text style={styles.title}>{gameData.name}</Text>
+          <Text style={styles.title}>{game.name}</Text>
         </View>
         <View style={styles.headerRow}>
           <TouchableOpacity 
@@ -124,12 +227,26 @@ export default function DancaCadeira ({ route, navigation }) {
       {/* Body */}
       <View style={styles.bodyContainer}>
         <View style={styles.bodyContent}>
-          
+          {
+            gaming ? (
+              <View>
+                <Text>Pausando em: {pausingIn}s</Text>
+                <Text>Cadeiras: {numChairs}</Text>
+              </View>
+            ) : null
+          }
+          {
+            timePaused > 0 ? (
+              <View>
+                <Text>Pausado por: {timePaused}s</Text>
+              </View>
+            ) : null
+          }
         </View>
         <View style={styles.bodyFooter}>
           <TouchableOpacity 
             style={styles.startGameButton} 
-            onPress={() => gaming ? stop() : play()} 
+            onPress={() => PlayGame()} 
             activeOpacity={0.6}
           >
             <Text style={styles.startGameButtonLabel}>{gaming ? 'PARAR' : 'INICIAR'}</Text>
